444c963129f528f097ac33685ea6c1428127b094
Javier Rodríguez <103605265+xavitico1@users.noreply.github.com>
2025-05-19 04:46:36 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
-bot_token = "7656468639:AAFZLaXapxnejcnsWnMlfK_m4zPQSvCvUKo"
+BOT_TOKEN = "7656468639:AAFZLaXapxnejcnsWnMlfK_m4zPQSvCvUKo"
 youtube_api_key = "AIzaSyAi3A8L4eYmxFrl6rwTAJxbBV1nlhOMDj0"
 download_directory = "downloads"
